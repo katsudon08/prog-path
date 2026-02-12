a2cc12ddb60269dde642ed04e7f785688eb25619
matsu <matsuaki171101@gmail.com>
2026-02-12 22:06:45 +0900

    chore: v4のtailwindcssを導入
    - v3とは打って変わり、cssベースだったため設定がとても楽でconfigのファイルもなくなっていた

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import { useState } from 'react';
-import './App.css';
-
-function App() {
-    const [count, setCount] = useState(0);
-
+export default function App() {
     return (
-        <>
+        <div className="text-3xl font-bold underline">
             <h1>ProgPath</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        </div>
     );
 }
-
-export default App;
